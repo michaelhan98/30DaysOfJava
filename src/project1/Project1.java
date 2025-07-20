319c8269460ffbcc1e4f032722ecb4d2cbb9ad4c
Michael Han <michaelhan9885@gmail.com>
2025-07-20 00:43:21 -0500

    initial commit

---
+++ src/project1/Project1.java
@@ -0,0 +1,47 @@
+package project1;
+import java.util.Scanner;
+
+public class Project1 {		
+
+	public static void main(String[] args) {	//parameter //this is a method
+		// TODO Auto-generated method stub
+		System.out.println("Enter in a name...");		//this is output 
+		
+		Scanner in = new Scanner(System.in);	//input a value 
+//		String s = in.nextLine();				//next line 
+		
+//		if(s.equals("Michael")) {
+//			System.out.println("Hey Michael");
+//		} else if (s.equals("John")) {
+//			System.out.println("Hey John");
+//		}	else System.out.println("Nice try buddy");
+	
+		
+		int i = 0;								//intitialization 
+		
+		do {
+			System.out.println("i is: " + i);
+			i++;
+		} while(false);
+		
+		//introduction to loops
+//		while(i < 10) {							//condition
+//			System.out.println("i is: " + i);
+//			i++;								//update
+//		}
+		
+//		for(int i = 0; i < 10; i++) {
+//			System.out.println("i is: "+ i);
+//		}
+		
+		in.close();
+		
+//		int a = 5;								//declaring an integer 
+//		char b = 't';							//declaring a character NOT string 
+//		
+//		System.out.println(s);				//output the input from line 10
+//		System.out.println(a);
+//		System.out.println(b);
+	}
+
+}
